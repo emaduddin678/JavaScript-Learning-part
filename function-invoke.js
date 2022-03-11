27a3fdd94bb4cbdaf3ef8faa36a4d76e64cfe577
emaduddin678 <emaduddin678@gmail.com>
2022-03-11 20:11:38 +0600

    2nd push

---
+++ function-invoke.js
@@ -0,0 +1,19 @@
+function functionName() {
+    console.log('This is first Function')
+}
+
+function sum() {
+    let a = 20
+    let b = 30 
+    console.log(a+b)
+}
+
+function sub() {
+    let a = 70
+    let b = 50
+    console.log(a - b)
+}
+
+functionName()
+sum()
+sub()
